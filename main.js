2fa5ad901cd9fe2a4b24b7dc841ac1316a7bb124
sanjar6767 <rasulberdievsanjar@gmail.com>
2025-11-08 18:24:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let ism = prompt("ismini yoz")
+
+let bod = document.body
+
+bod.style.backgroundColor = "blue"
+
+let h = document.getElementById("h")
+
+h.style.color = "yellow"
+h.style.fontSize = "100px"
+h.style.marginLeft = "777px"
+h.style.marginTop = "300px"
+
+h.innerHTML = ism
+
